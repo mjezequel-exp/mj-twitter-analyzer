f0c070d2a76f3cdf6da0d506a43a291321996eee
Maxime POIRON <mpoiron@expertime.com>
2025-02-04 15:43:37 +0000

    Merged PR 788: fix: create localhost cert only on dev server builds
    fix: create localhost cert only on dev server builds

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,32 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
-const certFilePath = "./localhost.pfx";
-if (!fs.existsSync(certFilePath)) {
-    const { status } = child_process.spawnSync("dotnet", ["dev-certs", "https", "--export-path", certFilePath, "--password", "localhost"], { stdio: "inherit" });
-    if (status !== 0) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), basicSsl()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command }) => {
+    if (command === "serve") {
+        // Create a self-signed certificate if it does not exist when running the dev server
+        const certFilePath = "./localhost.pfx";
+        if (!fs.existsSync(certFilePath)) {
+            if (0 !== child_process.spawnSync("dotnet", ["dev-certs", "https", "--export-path", certFilePath, "--password", "localhost"], { stdio: "inherit" }).status) {
+                throw new Error("Could not create certificate.");
+            }
+        }
+    }
+
+    return {
+        plugins: [vue(), basicSsl()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
         },
-    },
-    server: {
-        host: "localhost",
-        https: {
-            pfx: "./localhost.pfx",
-            passphrase: "localhost",
+        server: {
+            host: "localhost",
+            https: {
+                pfx: "./localhost.pfx",
+                passphrase: "localhost",
+            },
+            port: 44340,
         },
-        port: 44340,
-    },
+    };
 });
