d1485587a2a8d0a8b93cbbb54d4436b30d633e5a
Maxime JEZEQUEL <mjezequel@expertime.com>
2025-11-18 15:53:45 +0100

    Chore : remove useless files + update .env

--- src/components/welcome/icons/index.ts
+++
@@ -1,8 +0,0 @@
-import IconAzure from "./IconAzure.vue";
-import IconDocumentation from "./IconDocumentation.vue";
-import IconEcosystem from "./IconEcosystem.vue";
-import IconSupport from "./IconSupport.vue";
-import IconTooling from "./IconTooling.vue";
-import IconWeb from "./IconWeb.vue";
-
-export { IconAzure, IconDocumentation, IconEcosystem, IconSupport, IconTooling, IconWeb };
